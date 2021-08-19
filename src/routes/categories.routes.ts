3ed623542d8f0dd558099bc096b5fe204579c9a0
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-08-19 16:15:36 -0300

    criando repositores para categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,21 @@
-import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+import { request, response, Router } from "express";
 import { Category } from "../models/Category";
+import { CategoryRepository } from "../repositories/CategoryRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = new Category();
+    categoryRepository.create({ name, description });
 
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    })
-    categories.push(category);
-
-    return response.status(201).json({ category });
+    return response.status(201).send();
 });
+
+categoriesRoutes.get("/", (request, response) => {
+    const categoriesList = categoryRepository.list();
+
+    return response.json(categoriesList);
+})
 export { categoriesRoutes };
