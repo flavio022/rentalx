ad1dcf4edfeb8ea1c4a3137fd06cbbf9610987c1
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-09-15 11:51:10 -0300

    Refatorando rotas

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,4 +1,4 @@
-import { request, response, Router } from "express";
+import { Router } from "express";
 import { createSpecificaitionController } from "../modules/cars/usecases/createSpecification";
 
 
