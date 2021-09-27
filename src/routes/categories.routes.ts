d4615c7241da58b8729dbab568a9e9cf98898b2f
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-09-27 13:01:33 -0300

    adicionando multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,19 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
 import { createCategoryController } from "../modules/cars/usecases/createCategory";
 import { listCategoryController } from "../modules/cars/usecases/listCategory";
+import multer from 'multer';
+
+const upload = multer({
+    dest: "./tmp",
+})
 
 const categoriesRoutes = Router();
 
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
+})
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
 });
